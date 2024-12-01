7ad3cde892a3910e7d9ef67cba65e73fba5a27b3
Ted Flynn <ted@wolfco.us>
2024-12-01 10:46:59 -0600

    Very un-abstracted addition of likes and reposts.

---
+++ src/filters/likes-by-url.js
@@ -0,0 +1,16 @@
+export const likesByUrl = (webmentions, url) => {
+  const allowedTypes = ['like-of'];
+
+  const orderByDate = (a, b) => new Date(a.published) - new Date(b.published);
+
+  const checkRequiredFields = entry => {
+    const { author } = entry;
+    return !!author && !!author.name;
+  };
+
+  return webmentions
+    .filter(entry => entry['wm-target'] === url)
+    .filter(entry => allowedTypes.includes(entry['wm-property']))
+    .filter(checkRequiredFields)
+    .sort(orderByDate);
+};
